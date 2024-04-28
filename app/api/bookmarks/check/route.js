d846ad1ec4d1d6438600efab6c8ad5b54969cf3c
Farghana Hussain <farghana@gmail.com>
2024-04-28 01:06:57 -0400

    Check if user logged in and if the property is bookmarked

--- app/api/bookmarks/check/route.js
+++ app/api/bookmarks/check/route.js
@@ -8,31 +8,23 @@ export const dynamic = "force-dynamic";
 export const POST = async (request) => {
 	try {
 		await connectDB();
-		//get user
-		const sessionUser = await getSessionUser();
-		if (!sessionUser || !sessionUser.userId) {
-			return new Response("User Id is required", { status: 401 });
-		}
-		const { userId } = sessionUser;
-		// Find user in database
-		const user = await User.findOne({ _id: userId });
 		const { propertyId } = await request.json();
 
-		//check if user is bookmarked
-		let isBookmarked = user.bookmarks.includes(propertyId);
-		let message;
-		if (isBookmarked) {
-			//already bookmarked, remove it
-			user.bookmarks.pull(propertyId);
-			message = "Bookmark removed successfully";
-			isBookmarked = false;
-		} else {
-			user.bookmarks.push(propertyId);
-			message = "Bookmark added successfully";
-			isBookmarked = true;
+		const sessionUser = await getSessionUser();
+
+		if (!sessionUser || !sessionUser.userId) {
+			return new Response("User ID is required", { status: 401 });
 		}
-		await user.save();
-		return new Response(JSON.stringify({ message, isBookmarked }), {
+
+		const { userId } = sessionUser;
+
+		// Find user in database
+		const user = await User.findOne({ _id: userId });
+
+		// Check if property is bookmarked
+		let isBookmarked = user.bookmarks.includes(propertyId);
+
+		return new Response(JSON.stringify({ isBookmarked }), {
 			status: 200,
 		});
 	} catch (error) {
--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -9,6 +9,36 @@ const BookmarkButton = ({ property }) => {
 	const { data: session } = useSession();
 	const userId = session?.user?.id;
 	const [isBookmarked, setIsBookmarked] = useState(false);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		if (!userId) {
+			setLoading(false);
+			return;
+		}
+		const checkBookmarkStatus = async () => {
+			try {
+				const res = await fetch(`/api/bookmarks/check`, {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						propertyId: property._id,
+					}),
+				});
+				if (res.status === 200) {
+					const data = await res.json();
+					setIsBookmarked(data.isBookmarked);
+				}
+			} catch (error) {
+				console.error(error);
+			} finally {
+				setLoading(false);
+			}
+		};
+		checkBookmarkStatus();
+	}, [property._id, userId]);
 
 	const handleClick = async () => {
 		if (!userId) {
@@ -18,14 +48,17 @@ const BookmarkButton = ({ property }) => {
 		try {
 			const res = await fetch(`/api/bookmarks`, {
 				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
 				body: JSON.stringify({
 					propertyId: property._id,
 				}),
 			});
 			if (res.status === 200) {
 				const data = await res.json();
-				toast.success(data.message);
 				setIsBookmarked(data.isBookmarked);
+				toast.success(data.message);
 			}
 		} catch (error) {
 			console.error(error);
@@ -33,7 +66,17 @@ const BookmarkButton = ({ property }) => {
 		}
 	};
 
-	return (
+	if (loading) return <p className='text-center'>Loading...</p>;
+
+	return isBookmarked ? (
+		<button
+			onClick={handleClick}
+			className='bg-red-500 hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center'
+		>
+			<FaBookmark className='mr-1' />
+			Remove Bookmark
+		</button>
+	) : (
 		<button
 			onClick={handleClick}
 			className='bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center'
