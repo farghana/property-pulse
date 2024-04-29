3ade7e7a6a9cba519a2f942225742e9f9f60d212
Farghana Hussain <farghana@gmail.com>
2024-04-29 17:19:59 -0400

    Add search API

---
+++ app/api/properties/search/route.js
@@ -0,0 +1,40 @@
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+
+//GET /api/properties/search
+
+export const GET = async (request) => {
+	try {
+		await connectDB();
+		const { searchParams } = new URL(request.url);
+		const searchTerm = searchParams.get("search");
+		const propertyType = searchParams.get("type");
+
+        const searchPattern = new RegExp(searchTerm, 'i');
+
+        // Match location pattern against database fields
+        let query = {
+            $or: [
+                {name: searchPattern},
+                {description: searchPattern},
+                {'location.street': searchPattern},
+                {'location.city': searchPattern},
+                {'location.state': searchPattern},
+                {'location.zipcode': searchPattern},
+            ]
+        }
+
+        //only check for property type if it's not all
+        if(propertyType && propertyType !== 'All'){
+            const typePattern = new RegExp(propertyType, 'i');
+            query.type = typePattern;
+        }
+
+		const properties = await Property.find(query);
+
+		return new Response(JSON.stringify({message: "Success"}), { status: 200 });
+	} catch (error) {
+		console.log(error);
+		return new Response("Something went wrong", { status: 500 });
+	}
+};
