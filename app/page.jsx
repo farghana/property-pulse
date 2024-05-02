8588a4406340ceec84baea34292c59631608d29b
Farghana Hussain <farghana@gmail.com>
2024-05-02 16:28:38 -0400

    Add features property section to homepage

--- app/page.jsx
+++ app/page.jsx
@@ -2,11 +2,13 @@
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
 import HomeProperties from "@/components/HomeProperties";
+import FeaturedProperties from "@/components/FeaturedProperties";
 const Homepage = () => {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <FeaturedProperties />
       <HomeProperties />
     </>
   )
--- utils/requests.js
+++ utils/requests.js
@@ -1,11 +1,13 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
 //fetch all properties
-const fetchProperties = async () => {
+const fetchProperties = async ({ showFeatured = false } = {}) => {
 	try {
 		//handle the case where domain is not available yet
 		if (!apiDomain) return [];
-		const response = await fetch(`${apiDomain}/properties`);
+		const response = await fetch(
+			`${apiDomain}/properties${showFeatured ? "/featured" : ""}`
+		);
 		if (!response.ok) {
 			throw new Error("Failed to fetch data");
 		}
